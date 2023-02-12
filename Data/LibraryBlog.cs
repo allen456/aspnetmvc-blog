b89ea7e0d1eee1b75a49434c889a129bc680f278
allen456 <alen456@proton.me>
2023-02-13 06:09:46 +0800

    updated libraries

--- Data/LibraryBlog.cs
+++ Data/LibraryBlog.cs
@@ -2,6 +2,7 @@
 using aspnetmvc_blog.Models.Views;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace aspnetmvc_blog.Data
 {
@@ -20,6 +21,44 @@ namespace aspnetmvc_blog.Data
                 blogList = JsonConvert.DeserializeObject<List<BlogDataViewModel>>(apiResponse);
             }
             return blogList;
+        }
+
+        public async Task<BlogDataViewModel> getBlogData(string id)
+        {
+            BlogDataViewModel blogData = new();
+            using (var httpClient = new HttpClient())
+            {
+                using var response = await httpClient.GetAsync(_config.GetValue<string>("BlogApi") + "/" + id);
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                blogData = JsonConvert.DeserializeObject<BlogDataViewModel>(apiResponse);
+            }
+            return blogData;
+        }
+
+        public async Task<BlogDataViewModel> sendBlogData(BlogDataPostViewModel newBlogData)
+        {
+            BlogDataViewModel blogData = new();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new(JsonConvert.SerializeObject(newBlogData), Encoding.UTF8, "application/json");
+                using var response = await httpClient.PostAsync(_config.GetValue<string>("BlogApi"), content);
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                blogData = JsonConvert.DeserializeObject<BlogDataViewModel>(apiResponse);
+            }
+            return blogData;
+        }
+
+        public async Task<BlogDataViewModel> updateBlogData(string id, BlogDataPostViewModel newBlogData)
+        {
+            BlogDataViewModel blogData = new();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new(JsonConvert.SerializeObject(newBlogData), Encoding.UTF8, "application/json");
+                using var response = await httpClient.PatchAsync(_config.GetValue<string>("BlogApi") + "/" + id, content);
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                blogData = JsonConvert.DeserializeObject<BlogDataViewModel>(apiResponse);
+            }
+            return blogData;
         }
     }
 }
